92b9cc4a3ce596b3ba390603ec97e39a5be3d7cb
TheKoSmOnAvT <ni_popov@mail.ru>
2020-02-14 18:36:35 +0500

    validation on web

--- LOCS/server.js
+++ LOCS/server.js
@@ -1,29 +1,35 @@
 const express = require("express");
 const app = express();
 app.use(express.static('static'));
+const bodyParser = require("body-parser");
+// создаем парсер для данных application/x-www-form-urlencoded
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(function(request, response, next) {
-
+    /*
     let now = new Date();
     let hour = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
     let data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
     console.log(data);
-    console.log(data + request.url + "\n");
+    console.log(data + request.url + "\n");*/
     next();
 });
 app.get("/", function(request, response) {
 
     response.sendFile(__dirname + "/static/map.html");
 });
-app.get("/about", function(request, response) {
+app.get("/create_events", function(request, response) {
 
-    response.send("<h1>О сайте</h1>");
+    response.sendFile(__dirname + "/static/event_form.html");
 });
-app.get("/contact", function(request, response) {
+app.post("/create_events/new", urlencodedParser, function(request, response) {
 
-    response.send("<h1>Контакты</h1>");
+    response.redirect("..");
+    console.log(request.body.event_name);
+    console.log(request.body.event_tag);
+    console.log(request.body.evet_info);
 });
 
 app.listen(3000);
