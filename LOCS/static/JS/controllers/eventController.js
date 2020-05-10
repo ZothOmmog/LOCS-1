9e19c394ff9d9806b30fda7c77871b62863c998e
Nikita  Popov <ni_popov@mail.ru>
2020-05-10 14:05:01 +0500

    works on event func

--- LOCS/static/JS/controllers/eventController.js
+++ LOCS/static/JS/controllers/eventController.js
@@ -5,46 +5,49 @@ var DataBase = require('../scripts/DataBase.js');
 
 
 
-async function takeObj(token) {
-    let data;
-    await DataBase.TakeToken(token).then(function(val) {
-        data = val;
+// async function takeObj(token) {
+//     let data;
+//     await DataBase.TakeToken(token).then(function(val) {
+//         data = val;
+//     });
+//     return data;
+// }
+
+
+
+// exports.delete = async function(request, response) {
+//     const userId = request.cookies.userId ? await takeObj(request.cookies.userId).then(function(val) { return val.taketoken; }) : undefined;
+//     if (userId) {
+//         const Role = request.cookies.userRole ? await takeObj(request.cookies.userRole).then(function(val) { return val.taketoken; }) : undefined;
+//         //организатор или админ
+//         if (Role == 2 || Role == 0) {
+//             let idEvent = request.params.idEvent;
+//             var Check;
+//             await DataBase.deleteEvent(idEvent).then(function(val) {
+//                 Check = val;
+//             });
+//             response.json({ "Deleted": true });
+//         } else {
+//             response.json({ err: "do not have permissions" });
+//         }
+//     } else {
+//         response.json({ err: "user dont sing in" });
+//     }
+// };
+
+
+exports.event = async function(request, response) {
+    var idEvent = request.body.idEvent;
+    var tags;
+    var event;
+    await DataBase.EventTags(idEvent).then(function(val) {
+        tags = val;
     });
-    return data;
-}
 
-
-exports.create = function(request, response) {
-
-};
-
-exports.change = function(request, response) {
-
-};
-
-exports.delete = async function(request, response) {
-    const userId = request.cookies.userId ? await takeObj(request.cookies.userId).then(function(val) { return val.taketoken; }) : undefined;
-    if (userId) {
-        const Role = request.cookies.userRole ? await takeObj(request.cookies.userRole).then(function(val) { return val.taketoken; }) : undefined;
-        //организатор или админ
-        if (Role == 2 || Role == 0) {
-            let idEvent = request.params.idEvent;
-            var Check;
-            await DataBase.deleteEvent(idEvent).then(function(val) {
-                Check = val;
-            });
-            response.json({ "Deleted": true });
-        } else {
-            response.json({ err: "do not have permissions" });
-        }
-    } else {
-        response.json({ err: "user dont sing in" });
-    }
-};
-
-
-exports.event = function(request, response) {
-
+    await DataBase.event(idEvent).then(function(val) {
+        event = val;
+    });
+    response.json({ "event": event.event, tags });
 };
 
 //надо подумать о фильтре по тегам 
@@ -78,6 +81,66 @@ exports.shortList = async function(request, response) {
     response.json(Events);
 };
 
-exports.search = function(request, response) {
+exports.search = async function(request, response) {
+    let limit = request.params.limit;
+    let offset = (request.params.offset - 1) * limit;
+    offset = offset <= 0 ? 1 : offset;
+    limit = limit <= 0 ? 1 : limit;
 
+    var word = request.body.word;
+    var Events;
+    console.log(word);
+    await DataBase.searchEvent(word, limit, offset).then(function(val) {
+        Events = val;
+    });
+
+    console.log(Events);
+
+    for (i in Events) {
+        var tags;
+
+        await DataBase.EventTags(Events[i].searchevent.id).then(function(val) {
+            tags = val;
+        });
+        var masTags = [];
+
+        for (j in tags) {
+            masTags.push(tags[j].eventtags.title);
+        };
+        Events[i].tags = masTags;
+    };
+
+    response.json(Events);
+};
+
+
+
+
+exports.tags = async function(request, response) {
+    var tags;
+    await DataBase.tags().then(function(val) {
+        tags = val;
+    });
+
+    response.json(tags);
+
+};
+exports.tagsLim = async function(request, response) {
+
+    let limit = request.params.limit;
+    let offset = (request.params.offset - 1) * limit;
+    offset = offset <= 0 ? 1 : offset;
+    limit = limit <= 0 ? 1 : limit;
+
+    var count;
+    var tags;
+
+    await DataBase.CountTags().then(function(val) {
+        count = val;
+    });
+
+    await DataBase.tags(limit, offset).then(function(val) {
+        tags = val;
+    });
+    response.json({ "count": count.counttags.count, tags });
 };
--- LOCS/static/JS/routes/eventRouter.js
+++ LOCS/static/JS/routes/eventRouter.js
@@ -2,10 +2,18 @@ const express = require("express");
 const eventController = require("../controllers/eventController.js");
 const eventRouter = express.Router();
 
-eventRouter.use("/create$", eventController.create);
+//eventRouter.use("/create$", eventController.create);
 
 eventRouter.get("/list/:limit/:offset", eventController.shortList);
 
-eventRouter.get("/delete/:idEvent", eventController.delete);
+//eventRouter.get("/delete/:idEvent", eventController.delete);
 
+eventRouter.post("/info", eventController.event);
+
+eventRouter.get("/tags/:limit/:offset", eventController.tagsLim);
+
+eventRouter.get("/tags$", eventController.tags);
+
+
+eventRouter.post("/search/:limit/:offset", eventController.search);
 module.exports = eventRouter;
