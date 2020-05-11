cd0c7861f7506e02af043a457d52fef2a6a97dde
Host <paklin.2020@gmail.com>
2020-05-11 22:51:13 +0500

    Rework PagesNumbersMenu with button-bem

--- LOCS/frontend/src/components/PagesNumbersMenu/PagesNumbersMenu.jsx
+++ LOCS/frontend/src/components/PagesNumbersMenu/PagesNumbersMenu.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import s from './PagesNumbersMenu.module.scss';
 import '../CommonStyles/Button/Button.css';
-
+import { Button } from '../indexComponents.js';
 
 export const PagesNumbersMenu = (props) => {
-    
-    const pages = props.pages.map((page) => +page !== +props.currentPage ?
-        <div 
-            className={'button ' + s.PagesNumbersMenu__item}
-            onClick={props.changeCurrentPage}
-        >
-            {page}
-        </div> :
-        <div className={'button ' + s.PagesNumbersMenu__item + ' ' + s.PagesNumbersMenu__item_active}>
-            {page}
-        </div>
+    const pages = props.pages.map((page, index) => +page !== +props.currentPage ?
+        <Button buttonText={page} style='Switch' onClickHandler={props.changeCurrentPage} /> :
+        <Button buttonText={page} style='SwitchActive' />
     );
 
     return (
