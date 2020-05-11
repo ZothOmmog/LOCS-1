091bb43eda9dccec248d8658ca01c4c4fc3fa4bf
Host <paklin.2020@gmail.com>
2020-05-11 22:49:16 +0500

    Added Switch and Base button in button-bem

--- LOCS/frontend/src/components/Button-bem/Button.jsx
+++ LOCS/frontend/src/components/Button-bem/Button.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import style from './Button.module.scss';
+import s from './Button.module.scss';
 
 export const Button = (props) => {
+    let type = s.Button;
+
+    type += (
+        props.style === 'Switch' ? (' ' + s.Button_Style_Switch) :
+        props.style === 'SwitchActive' ? (' ' + s.Button_Style_SwitchActive) : (' ' + s.Button_Style_Base)
+    );
+
+    type += (
+        props.size === 'FullContainer' ? (' ' + s.Button_Size_FullContainer) : (' ' + s.Button_Size_Base)
+    );
+
     return (
-    <button 
-        className={style.button}
-        onClick={props.onClikHandler}
-    >
-        {props.buttonText}
-    </button>
+        <button
+            className={type}
+            onClick={props.onClickHandler}
+        >
+            {props.buttonText}
+        </button>
     )
 }
