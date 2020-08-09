796cf0192ab6dc1495dc6752db8f32acc227db55
Host <paklin.2020@gmail.com>
2020-08-09 23:50:59 +0500

    Подключен плагин для анализа циклов зависимостей

--- LOCS/frontend/craco.config.js
+++ LOCS/frontend/craco.config.js
@@ -1,15 +1,30 @@
 const path = require('path');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 const { alias } = require('./src/lib');
 
-const aliasForCraco = {}
+const aliasForCraco = {};
 
-for(let key in alias) {
-  aliasForCraco[key] = path.resolve(
-    __dirname, 
-    './src/' + alias[key]
-  );
+for (let key in alias) {
+    aliasForCraco[key] = path.resolve(__dirname, './src/' + alias[key]);
 }
 
 module.exports = {
-  webpack: { alias: { ...aliasForCraco } }
+    webpack: {
+        alias: { ...aliasForCraco },
+        plugins: [
+            new CircularDependencyPlugin({
+                // exclude detection of files based on a RegExp
+                exclude: /a\.js|node_modules/,
+                // include specific files based on a RegExp
+                include: /dir/,
+                // add errors to webpack instead of warnings
+                failOnError: true,
+                // allow import cycles that include an asyncronous import,
+                // e.g. via import(/* webpackMode: "weak" */ './file.js')
+                allowAsyncCycles: false,
+                // set the current working directory for displaying module paths
+                cwd: process.cwd(),
+            }),
+        ],
+    },
 };
