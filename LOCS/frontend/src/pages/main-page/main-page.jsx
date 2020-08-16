d95a7b72a8eb50e55d8ebf402dd5712758c2f6fa
Host <paklin.2020@gmail.com>
2020-08-16 20:15:16 +0500

    Произведен рефакторинг компонента главной страницы

--- LOCS/frontend/src/pages/main-page/main-page.jsx
+++ LOCS/frontend/src/pages/main-page/main-page.jsx
@@ -1,31 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { NavLinkMap } from '~/ui';
-import { eventProfileMockImg } from '~/assets';
 import { HeaderDefault } from '~/features/header-default';
 import { SelectedTagsList } from '~/features/selected-tags-list';
 import { MainTemplate } from '~/templates';
 import { EventListMain } from '~/features/event-list-main';
 
-const MainPageView = () => {
-    const eventListData = [];
-
-    return (
-        <MainTemplate   >
-            <HeaderDefault />
-            <SelectedTagsList />
-            <EventListMain />
-            <NavLinkMap to='/map' />
-        </MainTemplate>
-    );
-};
-
-const mapStateToProps = (state) => ({
-    isAuth: false,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-
-});
-
-export const MainPage = connect(mapStateToProps, mapDispatchToProps)(MainPageView);
+export const MainPage = () => (
+    <MainTemplate>
+        <HeaderDefault />
+        <SelectedTagsList />
+        <EventListMain />
+        <NavLinkMap to='/map' />
+    </MainTemplate>
+);
