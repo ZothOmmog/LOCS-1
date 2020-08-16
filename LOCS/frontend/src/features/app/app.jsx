e066b444c0ad6a20c3fafc1f6598efe2b281bb03
Host <paklin.2020@gmail.com>
2020-08-16 16:38:51 +0500

    Был перенесён компонент App в features

---
+++ LOCS/frontend/src/features/app/app.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Routes } from '~/pages';
+import { authSelectors, authThunks } from '~/redux/common-slices/auth-slice';
+import { useEffect } from 'react';
+import { Loader } from '../loader';
+
+function AppView({ isLoading }) {
+    
+
+    return (
+        <Loader isLoading={isLoading}>
+            <Routes />
+        </Loader>
+    );
+}
+
+export const App = () => {
+    const dispath = useDispatch();
+
+    const isLoading = useSelector(authSelectors.isLoadingSelector);
+
+    useEffect(() => {
+        dispath(authThunks.fetchAuth());
+    }, [dispath]);
+
+    return (
+        <AppView isLoading={isLoading} />
+    );
+}
--- LOCS/frontend/src/index.js
+++ LOCS/frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { App } from './App';
+import { App } from './features/app';
 
 ReactDOM.render(
     <React.StrictMode>
