f0fc8f46ef64310d75b1dfe89382a6da6b3a29f1
Host <paklin.2020@gmail.com>
2020-09-27 14:21:26 +0500

    Добавление к кастомной кнопки возможности прокидывать в неё реф

--- LOCS/frontend/src/ui/atoms/button-custom/button-custom.jsx
+++ LOCS/frontend/src/ui/atoms/button-custom/button-custom.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import ClassNames from 'classnames';
 import style from './style.module.scss';
 
-export const ButtonCustom = ({ onClick, className, children, ...outherProps}) => (
+export const ButtonCustom = ({ onClick, className, children, buttonRef, ...outherProps }) => (
     <button 
         onClick={onClick} 
         className={ClassNames(
@@ -11,13 +11,15 @@ export const ButtonCustom = ({ onClick, className, children, ...outherProps}) =>
             { [className]: className }
         )}
         {...outherProps}
+        ref={buttonRef}
     >
         {children}
     </button>
 );
 
 ButtonCustom.propTypes = {
-    onClick: PropTypes.func.isRequired,
+    onClick: PropTypes.func,
     className: PropTypes.string,
+    buttonRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
     children: PropTypes.node.isRequired
 };
