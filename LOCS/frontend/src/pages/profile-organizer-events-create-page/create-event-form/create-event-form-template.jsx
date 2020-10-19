4d6aa6bea549c5ba201055897907ae43635590e8
Host <paklin.2020@gmail.com>
2020-10-19 21:08:48 +0500

    Создан шаблон для формы, долделаны почти все инпуты

---
+++ LOCS/frontend/src/pages/profile-organizer-events-create-page/create-event-form/create-event-form-template.jsx
@@ -0,0 +1,28 @@
+import { Form } from 'formik';
+import React from 'react';
+import style from './create-event-form-template.module.scss';
+
+export const CreateEventFormTemplate = ({ children }) => (
+    <Form className={style['form']}>
+        <div className={style['__name-link']}>
+            {children[0]}
+            {children[1]}
+        </div>
+        <div className={style['__description-left']}>
+            {children[2]}
+            <div className={style['__right']}>
+                <div>
+                    <div className={style['__right-top']}>
+                        {children[3]}
+                        <div className={style['__right-top-date-price']}>
+                            {children[4]}
+                            {children[5]}
+                        </div>
+                    </div>
+                    {children[6]}
+                </div>
+                {children[7]}
+            </div>
+        </div>
+    </Form>
+);
--- LOCS/frontend/src/pages/profile-organizer-events-create-page/create-event-form/create-event-form.jsx
+++ LOCS/frontend/src/pages/profile-organizer-events-create-page/create-event-form/create-event-form.jsx
@@ -2,18 +2,33 @@ import React from 'react';
 import style from './create-event-form.module.scss';
 import * as Yup from 'yup';
 import classNames from 'classnames';
-import { Form, Formik } from 'formik';
+import { Formik } from 'formik';
 import { FormikInputCustom } from '~/features/formik-input-custom';
 import { FormikTextareaCustom } from '~/features/formik-textarea-custom';
-import { FormikCheckboxCustom } from '~/features/formik-checkbox-custom';
-import { FormikDatePicker } from '~/features/formik-date-picker';
+import { FormikDatePicker } from '~/features/formik-datepicker';
+import { FormikMultiselectCustom } from '~/features/formik-multiselect-custom';
+import { useSelector } from 'react-redux';
+import { tagsSelectors } from '~/redux/common-slices/tags-slice';
+import { CreateEventFormTemplate } from './create-event-form-template';
+import addDays from 'date-fns/addDays'
+import { ButtonColored } from '~/ui';
+
+const  REQUIRED_HINT = 'Обязательно для ввода';
 
 const loginSchema = Yup.object().shape({
-    login: Yup.string().email('Некорректный email').required('Обязательно для ввода'),
-    password: Yup.string().required('Обязательно для ввода')
+    idAddress: Yup.string().required(REQUIRED_HINT),
+    name: Yup.string().required(REQUIRED_HINT),
+    info: Yup.string().required(REQUIRED_HINT),
+    price: Yup.number(),
+    link: Yup.string(),
+    tags: Yup.array().min(1, 'Событие должно иметь от 1 до 5 тэгов').max(5, 'Можно выбрать не более 5 тэгов').required(),
+    date: Yup.date().typeError(REQUIRED_HINT).min(addDays(new Date(), 3), 'Не раньше, чем через 3 дня')
 });
 
 export const CreateEventForm = () => {
+    const tags = useSelector(tagsSelectors.tagsSelector);
+
+    const tagsForMultiselect = tags ? tags.map(tag => ({ value: tag.id, label: tag.name })) : [];
 
     return (
         <Formik
@@ -24,52 +39,40 @@ export const CreateEventForm = () => {
                 isPrise: false,
                 price: '',
                 link: '',
-                tags: '',
-                date: ''
+                tags: [],
+                date: null
             }}
             validationSchema={loginSchema}
             onSubmit={({ idAddress, name, info, price, link, tags, date }, { setSubmitting }) => {
                 console.error('Сабмит формы создания события не реализован')
             }}
         >
-            <Form className={style['form']}>
-                <div className={style['__name-link']}>
-                    <FormikInputCustom name='name' placeholder='Название события' className={classNames(
-                        style['__input'],
-                        style['__input-name']
-                    )}/>
-                    <FormikInputCustom name='link' placeholder='Ссылка на событие' className={classNames(
-                        style['__input'],
-                        style['__input-link']
-                    )}/>
-                </div>
-                <div className={style['__description-left']}>
-                    <FormikTextareaCustom name='info' placeholder='Описание' className={classNames(
-                        style['__input'],
-                        style['__input-description']
-                    )}/>
-                    <div className={style['__right']}>
-                        <div>
-                            {/* <FormikInputCustom name='date' placeholder='Дата' className={classNames(
-                                style['__input'],
-                                style['__input-date']
-                            )}/> */}
-                            <FormikDatePicker />
-                            <FormikInputCustom name='tags' placeholder='Тэги' className={classNames(
-                                style['__input'],
-                                style['__input-tags']
-                            )}/>
-                        </div>
-                        <div className={style['__prise']}>
-                            <FormikInputCustom name='price' placeholder='Стоимость' className={classNames(
-                                style['__input'],
-                                style['__input-prise']
-                            )}/>
-                            <FormikCheckboxCustom name='isPrise' className={style['__input-isPrise']} />
-                        </div>
-                    </div>
-                </div>
-            </Form>
+            <CreateEventFormTemplate>
+                <FormikInputCustom name='name' placeholder='Название события'
+                    wrapperClassName={classNames(style['__input'], style['__input-name'])}
+                />
+                <FormikInputCustom name='link' placeholder='Ссылка на событие (не обязательно)'
+                    wrapperClassName={classNames(style['__input'], style['__input-link'])}
+                />
+                <FormikTextareaCustom name='info' placeholder='Описание'
+                    className={classNames(style['__input'], style['__input-description'])}
+                    wrapperClassName={classNames(style['__input'], style['__input-description'])}
+                />
+                <FormikInputCustom name='idAddress' placeholder='Адрес проведения' 
+                    wrapperClassName={classNames(style['__input'], style['__input-address'])}
+                />
+                <FormikDatePicker name='date' placeholder='Дата проведения' />
+                <FormikInputCustom name='price' placeholder='Цена (не обязательно)'
+                    wrapperClassName={classNames(style['__input'], style['__input-prise'])}
+                />
+                <FormikMultiselectCustom items={tagsForMultiselect} name='tags' placeholder='Тэги' className={classNames(
+                    style['__input'],
+                    style['__input-tags']
+                )}/>
+                <ButtonColored type='Submit'>
+                    Создать
+                </ButtonColored>
+            </CreateEventFormTemplate>
         </Formik>
     );
 };
