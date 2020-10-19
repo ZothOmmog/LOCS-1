9f8c8a055a079ecbabcebb3c57a5235e4e0c7adc
Host <paklin.2020@gmail.com>
2020-10-19 21:04:24 +0500

    Удалена реализация чекбокса на основе material-ui

--- LOCS/frontend/src/features/formik-checkbox-custom/formik-checkbox-custom.jsx
+++ LOCS/frontend/src/features/formik-checkbox-custom/formik-checkbox-custom.jsx
@@ -1,15 +1,4 @@
-import React from 'react';
-import { Checkbox } from '@material-ui/core';
-import { useField } from 'formik';
-
 export const FormikCheckboxCustom = (props) => {
-    const [field] = useField({ ...props, type: 'checkbox' });
-
-    return (
-        <Checkbox
-            inputProps={{ 'aria-label': 'Чекбокс платного события' }}
-            {...field}
-            {...props}
-        />
-    );
+    console.error('Компонент FormikCheckboxCustom не реализован!');
+    return null;
 };
