bd94085a73829906e3996101c093e315a6492270
Host <paklin.2020@gmail.com>
2020-10-19 21:05:25 +0500

    Добавлен кастомный инпут для формика

--- LOCS/frontend/src/features/formik-input-custom/formik-input-custom.jsx
+++ LOCS/frontend/src/features/formik-input-custom/formik-input-custom.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import style from './formik-input-custom.module.scss';
 import classNames from 'classnames';
 import { useField } from 'formik';
+import { CustomInput } from '~/ui/atoms';
+import { FormikTextError } from '../formik-text-error';
 
-export const FormikInputCustom = ({ className, errorClassName, ...outher }) => {
+export const FormikInputCustom = ({ className, wrapperClassName, errorClassName, ...outher }) => {
     const [field, meta] = useField(outher);
 
     return (
-        <input 
-            className={classNames(
-                style['formik-input-custom'],
-                { [className]: className },
-                { [style['_error']]: meta.touched && meta.error && !errorClassName },
-                { [errorClassName]: meta.touched && meta.error && errorClassName}
-            )}
-            {...field}
-            {...outher}
-        />
+        <div className={classNames(style['_'], { [wrapperClassName]: wrapperClassName })}>
+            <CustomInput 
+                className={classNames(
+                    style['__input'],
+                    { [className]: className },
+                    { [style['_error']]: meta.touched && meta.error && !errorClassName },
+                    { [errorClassName]: meta.touched && meta.error && errorClassName}
+                )}
+                {...field}
+                {...outher}
+            />
+            <FormikTextError touched={meta.touched} error={meta.error} />
+        </div>
     );
 };
