9430f872658cba23dbe89b1feb0a84e603ec4a64
Host <paklin.2020@gmail.com>
2020-10-24 16:44:37 +0500

    Неудачная попытка в адаптив, надо будет всё допиливать

--- LOCS/frontend/src/features/profile-organizer-navbar/profile-organizer-navbar.jsx
+++ LOCS/frontend/src/features/profile-organizer-navbar/profile-organizer-navbar.jsx
@@ -10,32 +10,34 @@ import { useLocation } from 'react-router-dom';
 
 const ProfileOrganizerNavbarView = ({ logoutCallback, eventsPages, currentEventPage }) => (
     <div className={style['profile-organizer-navbar']}>
-        <NavLinkCustom to='/profile/organizer/info' className={style['__item']} activeClassName={style['__item_active']}>
-            Информация
-        </NavLinkCustom>
-        <ButtonWithDropList
-            componentsForList={eventsPages.map(
-                ([ name, path ]) => ({
-                    renderProp: className => (
-                        <LinkCustom to={path} className={classNames(style['__list-events-item'], className)}>
-                        {name}
-                        </LinkCustom>
-                    ),
-                    key: path
-                })
-            )}
-            className={style['__drop-list']}
-        >
-            {className => currentEventPage[1] ? (
-                <LinkCustom to={currentEventPage[1]} className={classNames(style['__item'], style['__item_active'], style['__item_button'], className)}>
-                    {currentEventPage[0]} 
-                </LinkCustom>
-            ) : (
-                <ButtonCustom className={classNames(style['__item'], style['__item_button'], className)}>
-                    {currentEventPage[0]}
-                </ButtonCustom>
-            )}
-        </ButtonWithDropList>
+        <div className={style['__left-content']}>
+            <NavLinkCustom to='/profile/organizer/info' className={style['__item']} activeClassName={style['__item_active']}>
+                Информация
+            </NavLinkCustom>
+            <ButtonWithDropList
+                componentsForList={eventsPages.map(
+                    ([ name, path ]) => ({
+                        renderProp: className => (
+                            <LinkCustom to={path} className={classNames(style['__list-events-item'], className)}>
+                            {name}
+                            </LinkCustom>
+                        ),
+                        key: path
+                    })
+                )}
+                className={style['__drop-list']}
+            >
+                {className => currentEventPage[1] ? (
+                    <LinkCustom to={currentEventPage[1]} className={classNames(style['__item'], style['__item_active'], style['__item_button'], className)}>
+                        {currentEventPage[0]} 
+                    </LinkCustom>
+                ) : (
+                    <ButtonCustom className={classNames(style['__item'], style['__item_button'], className)}>
+                        {currentEventPage[0]}
+                    </ButtonCustom>
+                )}
+            </ButtonWithDropList>
+        </div>
         <ButtonCustom className={style['__exit']} onClick={logoutCallback}>
             Выход
         </ButtonCustom>
