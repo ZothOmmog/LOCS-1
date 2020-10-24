421b142739b09e7ff1e71656192ae7c8cc5ac126
Host <paklin.2020@gmail.com>
2020-10-24 21:45:08 +0500

    Форма приведена в соответствие с новыми инпутами

--- LOCS/frontend/src/features/login/login.jsx
+++ LOCS/frontend/src/features/login/login.jsx
@@ -8,6 +8,7 @@ import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { ButtonBlack } from '~/ui/molecules';
 import { useDispatch } from 'react-redux';
 import { authThunks } from '~/redux/common-slices/auth-slice';
+import { FormikInputCustom } from '../formik-input-custom';
 
 const loginSchema = Yup.object().shape({
     login: Yup.string().email('Некорректный email').required('Обязательно для ввода'),
@@ -43,33 +44,11 @@ export const Login = () => {
                         setSubmitting(false);
                     }}
                 >
-                    {({ isSubmitting, errors, touched }) => (
+                    {({ isSubmitting }) => (
                         <Form>
                             <div className={style['__inner']}>
-                                <Field 
-                                    type='text'
-                                    name='login'
-                                    className={classNames(
-                                        style['__field'],
-                                        { [style['__field_error']]: errors.login && touched.login }
-                                    )}
-                                    placeholder='Логин'
-                                />
-                                <div className={style['__error-message']}>
-                                    <ErrorMessage name='login' component='div' className={style['__error-message-text']} />
-                                </div>
-                                <Field 
-                                    type='password'
-                                    name='password'
-                                    className={classNames(
-                                        style['__field'],
-                                        { [style['__field_error']]: errors.password && touched.password }
-                                    )}
-                                    placeholder='Пароль'
-                                />
-                                <div className={style['__error-message']}>
-                                    <ErrorMessage name='password' component='div' className={style['__error-message-text']}/>
-                                </div>
+                                <FormikInputCustom  name='login' placeholder='Логин' />
+                                <FormikInputCustom  name='password' placeholder='Пароль' type='password' className={style['__password']} />
                                 <ButtonBlack type="submit" disabled={isSubmitting} className={style['__submit']}>
                                     Войти
                                 </ButtonBlack>
