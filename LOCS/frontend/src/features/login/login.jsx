470440f9ce05c2cb4c7bbd0ad25e942496a0f3ff
Host <paklin.2020@gmail.com>
2020-10-25 14:22:49 +0500

    Переработка формы логина

--- LOCS/frontend/src/features/login/login.jsx
+++ LOCS/frontend/src/features/login/login.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import * as Yup from 'yup';
-import classNames from 'classnames';
 import style from './style.module.scss';
 import { ButtonColored } from '~/ui';
 import { PopupWindow } from '../popup-window';
-import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { Form, Formik } from 'formik';
 import { ButtonBlack } from '~/ui/molecules';
-import { useDispatch } from 'react-redux';
-import { authThunks } from '~/redux/common-slices/auth-slice';
+import { useDispatch, useSelector } from 'react-redux';
+import { authSelectors, authThunks } from '~/redux/common-slices/auth-slice';
 import { FormikInputCustom } from '../formik-input-custom';
+import { FormikTextError } from '../formik-text-error';
 
 const loginSchema = Yup.object().shape({
     login: Yup.string().email('Некорректный email').required('Обязательно для ввода'),
@@ -17,11 +17,11 @@ const loginSchema = Yup.object().shape({
 
 export const Login = () => {
     const [isOpenPopup, setIsOpenPopup] = useState(false);
+    const isAuth = useSelector(authSelectors.isAuthSelector);
+    const [errorLogin, setErrorLogin] = useState('');
 
     const dispatch = useDispatch();
-    const fetchLogin = (login, password) => {
-        dispatch(authThunks.fetchLogin({ login, password }));
-    };
+    const fetchLogin = (login, password) => dispatch(authThunks.fetchLogin({ login, password }));
 
     return (
         <div>
@@ -40,8 +40,11 @@ export const Login = () => {
                     initialValues={{ login: '', password: '' }}
                     validationSchema={loginSchema}
                     onSubmit={({ login, password }, { setSubmitting }) => {
-                        fetchLogin(login, password);
-                        setSubmitting(false);
+                        setSubmitting(true);
+                        fetchLogin(login, password).then(() => {
+                            if (!isAuth) setErrorLogin('Неверный логин или пароль');
+                            setSubmitting(false);
+                        });
                     }}
                 >
                     {({ isSubmitting }) => (
@@ -52,6 +55,7 @@ export const Login = () => {
                                 <ButtonBlack type="submit" disabled={isSubmitting} className={style['__submit']}>
                                     Войти
                                 </ButtonBlack>
+                                <FormikTextError error={errorLogin} touched={true} />
                             </div>
                         </Form>
                     )}
