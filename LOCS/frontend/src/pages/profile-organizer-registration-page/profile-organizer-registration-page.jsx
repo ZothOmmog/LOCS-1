f46f0f2b5a2ebdfc7ba2787ef0f760000b6a10fe
ZothOmmog <abpstd@gmail.com>
2020-10-31 17:22:37 +0500

    Написана заглушка для страницы регистрации организатора

---
+++ LOCS/frontend/src/pages/profile-organizer-registration-page/profile-organizer-registration-page.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ProfileOrganizerRegistrationTemplate } from '~/templates/profile-organizer-registration-template';
+
+export const ProfileOrganizerRegistrationPage = () => (
+    <ProfileOrganizerRegistrationTemplate>
+        <div>Тут регистрация организатора</div>
+    </ProfileOrganizerRegistrationTemplate>
+);
