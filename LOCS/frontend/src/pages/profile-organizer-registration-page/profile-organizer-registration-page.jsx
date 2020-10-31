76e2e0a97112b206c67241fd4d9bbaa03efa8c6a
ZothOmmog <abpstd@gmail.com>
2020-10-31 17:59:09 +0500

    Форма регистрации подключена на страницу

--- LOCS/frontend/src/pages/profile-organizer-registration-page/profile-organizer-registration-page.jsx
+++ LOCS/frontend/src/pages/profile-organizer-registration-page/profile-organizer-registration-page.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { ProfileOrganizerRegistrationTemplate } from '~/templates/profile-organizer-registration-template';
+import { RegistrationOrganizerForm } from './registration-organizer-form';
 
 export const ProfileOrganizerRegistrationPage = () => (
     <ProfileOrganizerRegistrationTemplate>
-        <div>Тут регистрация организатора</div>
+        <RegistrationOrganizerForm />
     </ProfileOrganizerRegistrationTemplate>
 );
