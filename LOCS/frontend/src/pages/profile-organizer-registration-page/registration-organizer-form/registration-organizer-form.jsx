e603e7f07313881b88b5e17404f27d1fd46eb06c
ZothOmmog <abpstd@gmail.com>
2020-11-04 16:40:13 +0500

    Подключена схема валидации к регистрации организатора

--- LOCS/frontend/src/pages/profile-organizer-registration-page/registration-organizer-form/registration-organizer-form.jsx
+++ LOCS/frontend/src/pages/profile-organizer-registration-page/registration-organizer-form/registration-organizer-form.jsx
@@ -6,11 +6,12 @@ import { ButtonColored } from '~/ui';
 import { RegistrationOrganizerFormTemplate } from './registration-organizer-form-template';
 import style from './registration-organizer-form.module.scss';
 import * as Yup from 'yup';
-
-
+import { MAX_HINT, REQUIRED_HINT } from '~/helpers/common-hints';
 
 const regOrgShema = Yup.object().shape({
-    organizationName: Yup.string().required('')
+    organizationName: Yup.string().required(REQUIRED_HINT),
+    organizationLink: Yup.string(),
+    info: Yup.string().required(REQUIRED_HINT).max(600, MAX_HINT(600))
 });
 
 export const RegistrationOrganizerForm = () => {
@@ -23,11 +24,12 @@ export const RegistrationOrganizerForm = () => {
                 organizationLink: '',
                 logo: ''
             }}
+            validationSchema={regOrgShema}
         >
             <RegistrationOrganizerFormTemplate>
                 <FormikInputCustom name='organizationName' wrapperClassName={style['__organizationName']} placeholder='Название организатора' />
-                <FormikInputCustom name='organizationLink' wrapperClassName={style['__organizationLink']} placeholder='Ссылка на сайт организатора' />
-                <FormikTextareaCustom name='info' wrapperClassName={style['__info']} placeholder='Описание организатора' />
+                <FormikInputCustom name='organizationLink' wrapperClassName={style['__organizationLink']} placeholder='Ссылка на сайт организатора (не обязательно)' />
+                <FormikTextareaCustom name='info' wrapperClassName={style['__info']} placeholder='Описание организатора (макс. 400 символов)' />
                 <ButtonColored type='submit'>Зарегистрировать</ButtonColored>
             </RegistrationOrganizerFormTemplate>
         </Formik>
