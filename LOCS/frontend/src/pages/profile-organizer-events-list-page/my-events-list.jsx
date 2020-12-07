9ee8a1e0b7d962b8d81aadf9fd1bcc63ca1ce1d0
ZothOmmog <abpstd@gmail.com>
2020-12-07 21:20:27 +0500

    Сделана подгрузка листа событий, добавлена пагинация

--- LOCS/frontend/src/pages/profile-organizer-events-list-page/my-events-list.jsx
+++ LOCS/frontend/src/pages/profile-organizer-events-list-page/my-events-list.jsx
@@ -1,9 +1,13 @@
+import { Pagination } from 'antd';
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { EventProfileSearch } from '~/ui/atoms';
-import { myEventsListThunks } from './my-events-list-slice';
+import { myEventsListSelectors, myEventsListThunks } from './my-events-list-slice';
+import styles from './my-events-list.module.scss';
 
 export const MyEventsList = () => {
+    const events = useSelector(myEventsListSelectors.selectAll);
+    const { pageSize, pageNumber, total } = useSelector(myEventsListSelectors.pageInfo);
 
     const dispatch = useDispatch();
     useEffect(() => {
@@ -11,11 +15,27 @@ export const MyEventsList = () => {
     }, [dispatch]);
 
     return (
-        <EventProfileSearch
-            name='Название события'
-            date='21.12.2020 19:00'
-            location='Улица Пушкина. Дом Колотушкина'
-            tags={[{ name: 'Кино' }, { name: 'Театр' }, { name: 'Гик' }, { name: 'Гик' }]}
-        />
+        <>
+            <div>
+                {events ? events.map(event => (
+                    <EventProfileSearch
+                        name={event.name}
+                        className={styles['item']}
+                        date='21.12.2020 19:00'
+                        location='Тестовая Улица. Тестовый дом'
+                        tags={[{ name: 'Мок 1' }, { name: 'Мок 2' }, { name: 'Мок подлиннее' }, { name: 'Просто мок' }]}
+                    />
+                )) : 'Нет данных'}
+            </div>
+            <div className={styles['pagination-wrapper']}>
+                <Pagination
+                    simple
+                    current={pageNumber}
+                    defaultPageSize={pageSize}
+                    total={total}
+                    onChange={(pageNumber) => dispatch(myEventsListThunks.fetchMyEvents(pageNumber))}
+                />
+            </div>
+        </>
     );
 };
